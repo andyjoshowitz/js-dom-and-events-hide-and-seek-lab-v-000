3d4d297713e185ae985f1a23ad8d2a223db6a5d3
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-04-25 18:39:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-view-source:https://learn.co/tracks/javascript-with-style
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  const grandNode = document.querySelector('#grand-node');
+  const children = grandNode.querySelectorAll('div');
+  return children[children.length-1]
+}
+function increaseRankBy(n) {
+  const rankings = document.querySelectorAll('.ranked-list li')
+  Array.prototype.map.call( rankings, function(i){
+    i.innerHTML = parseInt(i.innerHTML) + n
+  });
+}
